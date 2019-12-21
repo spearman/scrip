32ef0c91af433d768451acf7a2a98a36838438ea
Shane Pearman <spearman@github.com>
2019-12-21 04:00:22 -0800

    rewrite default.nix

--- default.nix
+++ default.nix
@@ -1,16 +1,7 @@
 with {
-  inherit (import <nixpkgs> {}) stdenv;
-  inherit (builtins) elemAt match readFile;
+  inherit (import <nixpkgs> {}) makeWrapper runCommand;
 };
-let
-  version-regex = ".*SCRIP_VERSION=\"([0-9]\\.[0-9]\\.[0-9])\".*";
-  version = elemAt (match version-regex (readFile ./bin/scrip)) 0;
-in
-stdenv.mkDerivation {
-  name = "scrip-" + version;
-  src  = ./bin;
-  installPhase = ''
-    mkdir -p $out/bin/
-    cp ./scrip $out/bin/
-  '';
-}
+runCommand "scrip" { buildInputs = [ makeWrapper ]; }
+''
+  makeWrapper ${./scrip.sh} $out/bin/scrip
+''
